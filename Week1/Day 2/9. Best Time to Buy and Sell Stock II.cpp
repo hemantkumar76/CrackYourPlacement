33c917283d6a27570206aacee044cb52dd3e61aa
Hemant <92645502+hemantkumar76@users.noreply.github.com>
2024-07-15 23:34:28 +0530

    Create 9. Best Time to Buy and Sell Stock II.cpp

---
+++ Week1/Day 2/9. Best Time to Buy and Sell Stock II.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit = 0;
+
+        // Loop through the prices from the second day
+        for (int i = 1; i < prices.size(); i++) {
+            // Agar aaj ka price kal se zyada hai, toh hum stock khareedte hain kal
+            // Aur bechte hain aaj, aur profit mein add kar dete hain difference
+            if (prices[i] > prices[i - 1]) {
+                profit += prices[i] - prices;
+            }
+        }
+
+        // Return the total profit
+        return profit;
+    }
+};
