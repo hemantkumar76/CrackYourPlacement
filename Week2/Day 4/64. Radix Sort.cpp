7fbcd8637b7fd9a95b22cb8f650fbdf67bd81a81
Hemant <92645502+hemantkumar76@users.noreply.github.com>
2024-07-26 02:49:29 +0530

    Create 64. Radix Sort.cpp

---
+++ Week2/Day 4/64. Radix Sort.cpp
@@ -0,0 +1,86 @@
+// C++ implementation of Radix Sort
+
+#include <iostream>
+
+using namespace std;
+
+// Ek utility function jo array mein maximum
+// value ko return karta hai
+int getMax(int arr[], int n)
+{
+    int mx = arr[0];
+    for (int i = 1; i < n; i++)
+        if (arr[i] > mx)
+            mx = arr[i];
+    return mx;
+}
+
+// Ek function jo counting sort karta hai arr[]
+// ke according to the digit
+// jo exp se represent kiya gaya hai.
+void countSort(int arr[], int n, int exp)
+{
+
+    // Output array
+    int output[n];
+    int i, count[10] = { 0 };
+
+    // Store count of occurrences
+    // in count[]
+    for (i = 0; i < n; i++)
+        count[(arr[i] / exp) % 10]++;
+
+    // Change count[i] so that count[i]
+    // now contains actual position
+    // of this digit in output[]
+    for (i = 1; i < 10; i++)
+        count[i] += count[i - 1];
+
+    // Output array ko build karte hain
+    for (i = n - 1; i >= 0; i--) {
+        output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+        count[(arr[i] / exp) % 10]--;
+    }
+
+    // Output array ko arr[] mein copy karte hain,
+    // so that arr[] now contains sorted
+    // numbers according to current digit
+    for (i = 0; i < n; i++)
+        arr[i] = output[i];
+}
+
+// The main function jo arr[] ko sort karta hai
+// of size n using Radix Sort
+void radixsort(int arr[], int n)
+{
+
+    // Maximum number ko find karte hain
+    // to know number of digits
+    int m = getMax(arr, n);
+
+    // Har digit ke liye counting sort karte hain.
+    // Note that instead of passing digit
+    // number, exp is passed. exp is 10^i
+    // where i is current digit number
+    for (int exp = 1; m / exp > 0; exp *= 10)
+        countSort(arr, n, exp);
+}
+
+// Ek utility function jo array print karta hai
+void print(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+}
+
+// Driver Code
+int main()
+{
+    int arr[] = { 170, 45, 75, 90, 802, 24, 2, 66 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    // Function Call
+    radixsort(arr, n);
+    print(arr, n);
+    return 0;
+}
