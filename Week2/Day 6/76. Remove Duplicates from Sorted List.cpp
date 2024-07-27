7192037764f78b36f770504a81785c9ab13d3b1c
Hemant <92645502+hemantkumar76@users.noreply.github.com>
2024-07-28 02:50:30 +0530

    Create 76. Remove Duplicates from Sorted List.cpp

---
+++ Week2/Day 6/76. Remove Duplicates from Sorted List.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    struct ListNode* deleteDuplicates(struct ListNode* head) {
+        struct ListNode* k = head;
+        struct ListNode* z;
+        if (head == NULL)
+            return head;
+        int i;
+        for (i = 0; i < 5; i++) {
+            while (k->next != NULL) {
+                z = k;
+                k = k->next;
+                if (z->val == k->val) {
+                    z->next = k->next;
+                }
+            }
+            k = head;
+        }
+        return head;
+    }
+};
