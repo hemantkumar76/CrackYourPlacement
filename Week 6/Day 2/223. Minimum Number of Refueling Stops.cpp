aa34ff19df54884bb3b1cd38a3b1b120ed6c473b
Hemant <92645502+hemantkumar76@users.noreply.github.com>
2024-08-20 19:54:33 +0530

    Create 223. Minimum Number of Refueling Stops.cpp

---
+++ Week 6/Day 2/223. Minimum Number of Refueling Stops.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+	int minRefuelStops(int target, int startFuel, vector<vector<int>>& stations) {
+		stations.push_back({target,0});
+		int min_stops = 0;
+		int cur_fuel = startFuel;
+		int cur_pos = 0;
+		priority_queue<int> optional;
+		for(int i = 0; i < stations.size();i++){
+			int required_fuel = stations[i][0] - cur_pos;
+			// iagar chaiye to lelo khi aur se 
+			while(cur_fuel < required_fuel && !optional.empty()){
+				int top_fuel = optional.top();
+				optional.pop();
+				cur_fuel += top_fuel;
+				min_stops++;
+			}
+			if(required_fuel <= cur_fuel){
+				cur_fuel -= required_fuel;
+				cur_pos += required_fuel;
+			}
+			else return -1;
+			// gar pahuch gaye to add kardo 
+			optional.push(stations[i][1]);
+		}
+		return min_stops;
+	}
+};
