0548c8dfd1645700d6561227a409550553a65e99
Hemant <92645502+hemantkumar76@users.noreply.github.com>
2024-08-22 01:39:46 +0530

    Create 232. Maximize The Cut Segments.cpp

---
+++ Week 6/Day 3/232. Maximize The Cut Segments.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+    public:
+   int solve(int n, int x, int y, int z, vector<int>&dp){
+        
+        if(n<0) return INT_MIN;
+        if(n==0) return 0;
+        
+        if(dp[n]!=-1) return dp[n];
+        
+        
+        int cl1=solve(n-x, x, y, z, dp);
+        int cl2=solve(n-y, x, y, z, dp);
+        int cl3=solve(n-z, x, y, z, dp);
+        
+        return dp[n]=1+max(cl1, max(cl2, cl3));
+    }
+    
+    int maximizeTheCuts(int n, int x, int y, int z)
+    {
+        vector<int>dp(n+1, -1);
+        return max(0, solve(n, x, y, z, dp));
+    }
+};
